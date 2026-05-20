cf31f1e63941a82c2e00a830be5881a4f30975bf
Codester02 <codefinder02@gmail.com>
2026-05-20 14:55:57 -0500

    fastfetch

--- .zshrc
+++ .zshrc
@@ -102,3 +102,7 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+#
+alias neofetch="pokeget lycanroc --form midnight -s --hide-name | fastfetch --logo-padding-top 10 --logo-padding-right 7 --logo-padding-left 6 --file-raw -"
+
+neofetch
--- install.sh
+++ install.sh
@@ -5,28 +5,33 @@ set -e
 DOTFILES_DIR="$HOME/.dotfiles"
 
 echo "Installing dotfiles..."
-
-sudo pacman -S --needed neovim kitty git firefox base-devel discord telegram-desktop steam spotify-player python-pipx
+echo "Installing pacman apps"
+sudo pacman -S --needed neovim kitty git firefox base-devel discord telegram-desktop steam spotify-player python-pipx fastfetch
 
 #setup Aur
+echo "AUR setup"
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ~/"$DOTFILES_DIR"
 
-yay -S noctalia-shell
+echo "installing AUR apps"
+yay -S --needed noctalia-shell pokeget
 # Create config dirs
 mkdir -p ~/.config
 
+echo "installing ohmyzsh"
 #oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #pywalfox
+echo "Installing pywalfox"
 pipx install pywalfox
 pywalfox install
 
 # Symlink configs
+echo "configuring symlinks"
 ln -sf "$DOTFILES_DIR/nvim" ~/.config/nvim
 ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
 ln -sf "$DOTFILES_DIR/hypr" ~/.config/hypr
